148f6e5ef7acb0c97fa44cb25c453b5dd00f69d8
Johnja85 <75758263+Johnja85@users.noreply.github.com>
2021-11-08 20:37:18 -0500

    Se agrega modelo para factura en el controlador y reenviarlas

--- app/Http/Controllers/integracionecomweb/ReenvioPedidoController.php
+++ app/Http/Controllers/integracionecomweb/ReenvioPedidoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\integracionecomweb;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\EncabezadoPedidoModel;
+use App\Models\EncabezadoFacturaModel;
 use App\Models\LogErrorImportacionModel;
 use Log;
 
@@ -15,11 +16,9 @@ class ReenvioPedidoController extends Controller
         $this->middleware('auth');
     }
 
-    
     public function index(Request $request)
     {
-// dump($request->all());
-
+        // dump($request->all());
         $buscar =$request->input('buscar');
         $registrosXpagina=100;
         if(!empty($buscar)){
@@ -28,12 +27,24 @@ class ReenvioPedidoController extends Controller
             $pedidosError = EncabezadoPedidoModel::where('estadoenviows', '=', "3")->paginate($registrosXpagina);
         }
         
-        // dump($pedidosError);
         return view('reenviopedido.index',compact(['pedidosError','buscar']));
     }
 
-    public function reenviarPedido(Request $request){        
+    public function indexfac(Request $request)
+    {
+        $buscar =$request->input('buscar');
+        $registrosXpagina=100;
+        if(!empty($buscar)){
+            $facturasError = EncabezadoFacturaModel::where('estadoenviows', '=', "3")->where('numero_factura','=',$buscar)->paginate($registrosXpagina);
+        }else{
+            $facturasError = EncabezadoFacturaModel::where('estadoenviows', '=', "3")->paginate($registrosXpagina);
+        }
+        
+        return view('reenviofacturas.index',compact(['facturasError','buscar']));
+    }
 
+    public function reenviarPedido(Request $request)
+    {        
         try {
             $pedido= $request->input('pedido');
             $arrayLlave= explode("|",$pedido);
@@ -58,8 +69,34 @@ class ReenvioPedidoController extends Controller
                 'renviado' => false
             ], 200);
         }
-        
+    }
 
+    public function reenviarFactura(Request $request)
+    {
+        try {
+            $factura= $request->input('factura');
+            $arrayLlave= explode("|",$factura);
+    
+            // Log::info($arrayLlave);
+            $centroOperacion= $arrayLlave[2];
+            $numeroFactura   = $arrayLlave[0];
+            $bodega= $arrayLlave[3];
+            $tipoDocumento= $arrayLlave[1];
+            $estado="0";
+    
+            $updateFactura= new LogErrorImportacionModel();
+            $updateFactura->actualizarEstadoDocumentoFac("",$estado,$centroOperacion,$bodega,$tipoDocumento,$numeroFactura);
+    
+            return response()->json([
+                'mensaje'=>'Factura actualizada',
+                'renviado' => true
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'mensaje'=>'Error al actualizar factura',
+                'renviado' => false
+            ], 200);
+        }  
     }
 
     public function getEncabezadoPedidoModel(){
